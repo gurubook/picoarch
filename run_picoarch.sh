c62605eb0621f35bb54a394cebcd0f65a13253c6
Luca Sacchi <gurubook@gmail.com>
2025-12-20 17:53:18 +0100

    fixed run script

--- run_picoarch.sh
+++ run_picoarch.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 export SDL_VIDEODRIVER="fbcon"
 export SDL_FBACCEL=0
 export SDL_FBDEV="/dev/fb0"
 export SDL_NOMOUSE=1
 export SDL_AUDIODRIVER=alsa
-picoarch $@
+picoarch.bin $@
